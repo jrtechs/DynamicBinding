9b9eb0a70b38b61fcb498f3c0fe81b316c69e748
bayleebooper <>
2016-05-24 08:14:35 -0400

    5/24/16  Worked on Living class -- kshaw/bayleebooper

--- DynamicBinding/src/dynamicbinding/Living.java
+++ DynamicBinding/src/dynamicbinding/Living.java
@@ -8,5 +8,29 @@ package dynamicbinding;
 
 public abstract class Living extends RotationalElement
 {
+    private int health;
+    private boolean isAlive;
     
+    
+    public Living()
+    {
+        super();
+        loadImage();
+        health=100;
+        isAlive=true;
+    }
+    
+    
+    public void takeDamage(RotationalElement elem)
+    {
+        if (elem instanceof Enemy)
+        {
+            health-=10;
+            
+            if (health==0)
+            {
+                isAlive=false;
+            }
+        }
+    }
 }
