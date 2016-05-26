e9e4f5d7c03d515a67fb54d72ea76b7640d9af10
Benh950 <bhilderbrandt@gmail.com>
2016-05-26 14:40:50 -0400

    Created player class Ben Chris Lexi

--- DynamicBinding/src/dynamicbinding/DynamicBinding.java
+++ DynamicBinding/src/dynamicbinding/DynamicBinding.java
@@ -14,8 +14,25 @@
 */
 package dynamicbinding;
 
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 public class DynamicBinding 
 {
+    //fields
+    public JFrame frame;
+    public JPanel panel;
+    ArrayList<Enemy> enemy;
+    ArrayList<Bullet> bullets;
+    Player p;
+    Map map;
+    
+    Timer move;//moves elements
+    Timer time;
+            
 
     public static void main(String[] args) 
     {
@@ -30,7 +47,92 @@ public class DynamicBinding
     
     private class Player extends Living
     {
+        //private Arraylist<Items> items;
+        boolean up = false;
+        boolean down = false;
+        boolean left = false;
+        boolean right = false;
         
+        public Player()
+        {
+            width = 50;
+            height = 50;
+            x=frame.getWidth()/2-this.width/2;
+            y=frame.getHeight()/2-this.height/2;
+            imageLocation = "player.png";
+            super.loadImage();
+        }
+        public void move()
+        {
+            if(down == true)
+            {
+                direction = 270;
+                super.move(-1);
+            }
+            if(up == true)
+            {
+                direction = 90;
+                super.move(-1);
+            }
+            if(left == true)
+            {
+                direction = 180;
+                super.move(-1);
+            }
+            if(right == true)
+            {
+                direction = 0;
+                super.move(-1);
+            }
+        }
+        public void updateDirection(KeyEvent e, boolean b)
+        {
+            if(e.getKeyCode()== KeyEvent.VK_W)
+            {
+                up = b;
+            }
+            if(e.getKeyCode()== KeyEvent.VK_A)
+            {
+                left = b;
+            }
+            if(e.getKeyCode()== KeyEvent.VK_S)
+            {
+                down = b;
+            }
+            if(e.getKeyCode()== KeyEvent.VK_D)
+            {
+                right = b;
+            }
+            if(e.getKeyCode()==KeyEvent.VK_UP)
+            {
+               shoot(90);
+            }
+            if(e.getKeyCode()==KeyEvent.VK_DOWN)
+            {
+                shoot(270);
+            }
+            if(e.getKeyCode()==KeyEvent.VK_LEFT)
+            {
+                shoot(180);
+            }
+            if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+            {
+                shoot(0);
+            }
+            
+        }
+        
+        public void shoot(int b)
+        {
+            Bullet bu = new Bullet();
+            bu.direction = b;
+            bullets.add(bu);
+        }
+            
+             
+            
+            
+
     }
     
     private class Bullet extends RotationalElement
