efda8e2e1b773fecc55f562e01b74bfce6b23725
TyronasaurusAN <17anoel@ga.hiltoncsd.net>
2016-05-26 13:59:57 -0400

    Created enemy class nerds. ayyy.
    Alex, Jared, Joe, and Willis.

--- DynamicBinding/src/dynamicbinding/Enemy.java
+++ DynamicBinding/src/dynamicbinding/Enemy.java
@@ -5,7 +5,29 @@
 */
 package dynamicbinding;
 
+import javax.swing.JFrame;
+
 public abstract class Enemy extends Living
 {
-    
+    public void spawn(JFrame j) 
+    {
+        x = (int) (Math.random() * j.getWidth());
+        y = (int) (Math.random() * j.getHeight());
+        
+    }
+    public void spawn(Living l)
+    {
+        x = l.x;
+        y = l.y;
+    }
+    public double distanceToPlayer(RotationalElement e) 
+    {
+        int xdif = x - e.x;
+        int ydif = y - e.y;
+        return (Math.sqrt(Math.pow(xdif,2) + Math.pow(ydif,2)));
+    }
+    public double angleToPlayer(RotationalElement e) 
+    {
+        return (Math.atan2((y - e.y),(x - e.x)));
+    }
 }
