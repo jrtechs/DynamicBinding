10af97d5e5d20a47fcd8a0f7f2966fee4ce9c89b
Benh950 <bhilderbrandt@gmail.com>
2016-06-02 13:53:18 -0400

    Take Damage Update Ben Chris Lexi

--- DynamicBinding/src/dynamicbinding/Living.java
+++ DynamicBinding/src/dynamicbinding/Living.java
@@ -6,23 +6,49 @@
 */
 package dynamicbinding;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 public abstract class Living extends RotationalElement
 {
-    private double health;
-    private boolean isAlive;
-    
+    public double health;
+    public boolean isAlive;
+    public boolean takeDamage;
+    public int cool; //Me
     
     public Living()
     {
-       health = 3;
+       ActionListener cooldown = new ActionListener()
+       {
+           public void actionPerformed(ActionEvent e)
+           {
+               if(takeDamage == false)
+               {
+                   cool ++;
+                   if(cool == 2)
+                   {
+                       takeDamage = true;
+                       cool = 0;
+                   }
+               }
+           }
+       };
+       Timer t = new Timer(1000, cooldown);
+       t.start();
+               
        isAlive= true;
+       takeDamage=true;
     }
     
     public void takeDamage(RotationalElement elem)
     {
-        if(isAlive)
+        if(isAlive && takeDamage)
         {
-            health-=.5;
+
+            health-=elem.damageGive;
+            takeDamage = false;
+            
             if(health <=0)
             {
                 isAlive = false;
